6eb15374725bd0017b9d7d728366edacce561364
RAJUCHODHARY <141802707+RAJUCHODHARY@users.noreply.github.com>
2024-04-24 18:26:38 +0530

    second

--- script.js
+++ script.js
@@ -3,87 +3,72 @@ let box = document.querySelector('.box');
 let box1 = document.querySelector('.box1');
 let box2 = document.querySelector('.box2');
 let box3 = document.querySelector('.box3');
-let imagearea = document.querySelector(".imagearea")
-
+let imagearea = document.querySelector(".imagearea");
 btn.addEventListener('click', coll);
-
+let arr = ["thor1.png", "hulk.jpg", "spider.jpg"];
 function coll() {
-    box1.classList.add('box2');
-    box1.classList.remove('box1');
+    box1.remove();
     box2.classList.remove('box2');
-    box2.classList.add('box1')
-    let div = document.createElement('div');
-    div.classList.add('div');
-    let img1 = document.createElement('img');
-    img1.src = "thor.png";
-    img1.classList.add('img1');
-    div.append(img1)
-    let btn1 = document.createElement('button');
-    btn1.innerText = "start game"
-    btn1.classList.add('btn')
-    div.append(btn1);
-    box2.append(div);
-    btn1.addEventListener('click', add);
+    box2.classList.add('box1');
+    for (let i = 0; i < 3; i++) {
+        let div = document.createElement('div');
+        div.classList.add('div');
+        let img1 = document.createElement('img');
+        img1.src = arr[i];
+        img1.classList.add('img1');
+        div.append(img1);
+        box2.append(div);
+    }
+    box2.children[0].addEventListener('click', add);
+    box2.children[1].addEventListener('click', add);
+    box2.children[2].addEventListener('click', add);
 }
-
-
-let clears,h1,htag;
-
-function add() {
-    box2.classList.add('box2');
-    // box3.classList.remove('box3');
-    box3.classList.add('imagearea')
-    
-     h1=document.createElement('h1')
-     htag=document.createElement('h1');
-     box3.append(htag)
-    box3.append(h1);
-    
-    
-     clears = setInterval(hello, 1000)
+let clears, h1, htag, imgs;
+function add(e) {
+    box2.remove();
+    box3.classList.add('imagearea');
+    let pera = document.createElement('p')
+    pera.classList.add('pera');
+    h1 = document.createElement('h1');
+    htag = document.createElement('h1');
+    pera.append(htag);
+    pera.append(h1);
+    box3.append(pera);
+    imgs = e.target.getAttribute("src");
+    clears = setInterval(hello, 1000);
 }
-
-let counts = 0,num=0;
-
+let counts = 0, num = 0;
 function hello() {
     counts++;
-    const x=box3.clientHeight;
-        const y=box3.clientWidth;
-        console.log(x,y)
+    const y = box3.clientHeight;
+    const x = box3.clientWidth;
     if (counts <= 60) {
-                    
-       
-        h1.innerText=counts;
-       let  icon = document.createElement('img');
-        icon.src = "thor1.png"
+        h1.innerText = ` Total = ${counts}`;
+        let icon = document.createElement('img');
+        icon.src = imgs;
         icon.classList.add('icon')
-        icon.style.top=getrand(x,y)[1]+"px";
-        icon.style.left=getrand(x,y)[0]+"px";
+        icon.style.top = getrand(x, y)[1] + "px";
+        icon.style.left = getrand(x, y)[0] + "px";
         box3.append(icon);
-       
-        icon.addEventListener('click',colls)
+        icon.addEventListener('click', colls);
     }
-    if (counts === 20) {
+    if (counts === 6) {
+        box3.remove();
         clearInterval(clears);
-        
-        let divs=document.createElement('div');
-        divs.innerText="Game over thank you"
-        divs.classList.add('div1')
+        let divs = document.createElement('div');
+        divs.innerText = "Game over thank you";
+        divs.classList.add('div1');
         box.append(divs);
-        }
-
+        box.classList.add('box1')
+    }
 }
-
-function getrand(x,y){
-const xcoord=Math.random() * x;
-const ycoord=Math.random() * y;
-console.log(xcoord,ycoord)
-return [xcoord,ycoord]
-    
+function getrand(x, y) {
+    const xcoord = Math.random() * x;
+    const ycoord = Math.random() * y;
+    return [xcoord, ycoord];
 }
-
-function colls(evet){
+function colls(evet) {
     num++;
-    htag.innerText=num;
+    htag.innerText = `Score = ${num}`;
     evet.target.remove();
 }
